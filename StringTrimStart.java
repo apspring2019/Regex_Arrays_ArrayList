b1fe86fada029ae1b00d4f2a8c67de8eaf2f9f43
Mohammad Omidvar Tehrani <mohammad.omt99@yahoo.com>
2019-03-05 19:00:27 +0330

    Add StringTrimStart

---
+++ StringTrimStart.java
@@ -0,0 +1,24 @@
+public class StringTrimStart
+{
+    public static void main(String[] args)
+    {
+        System.out.println(trimStringStart("xxxxxsalam", 'x'));
+        System.out.println(trimStringStart("        hello"));
+    }
+
+    private static String trimStringStart(String str)
+    {
+        return str.replaceFirst("^\\s*", "");
+    }
+
+    /***
+     * Returns a string without any leading character in the list
+     * @param str the string to be trimmed
+     * @param chars the list of characters
+     * @return
+     */
+    private static String trimStringStart(String str, char... chars)
+    {
+        return str.replaceFirst("^[" + new String(chars) + "]*", "");
+    }
+}
